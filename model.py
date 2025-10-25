26cc1ea942855fff947e095b99fd76db538e2822
Sharanya-sys <thantrysharanya@gmail.com>
2025-10-25 23:54:54 +0530

    Comment out TensorFlow import in model.py
    Comment out the import statement for TensorFlow.

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import tensorflow as tf   
+#import tensorflow as tf   
 def predict_disease(img_path):
       return "Basic Diagnosis: Check for visible spots; consult expert."
 from tensorflow.keras.preprocessing import image   
