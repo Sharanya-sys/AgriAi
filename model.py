602735bcf54aa10e74a2cfb9c7aa4fe9475475b1
Sharanya-sys <thantrysharanya@gmail.com>
2025-10-25 23:27:49 +0530

    Remove type ignore comments from model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
-import tensorflow as tf   #type: ignore
-from tensorflow.keras.preprocessing import image   #type: ignore
-import numpy as np   #type: ignore
+import tensorflow as tf   
+from tensorflow.keras.preprocessing import image   
+import numpy as np  
 
 model = tf.keras.applications.MobileNetV2(weights='imagenet')
 
